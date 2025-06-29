yyliu12/journalaccess-backend

commit 1d52b00f10786aa808dd97169046dea0893e4111

Limit Solr query highlight size

src/main/java/com/info25/journalindex/controllers/FileCrud.java
@@ -150,25 +150,15 @@ public class FileCrud {
 
             // attempt to read both a.pdf.txt and a.txt (as an example)
             String content = "";
-            String txtName = file.getOriginalFilename() + ".txt";
+            String[] attemptedContent = new String[3];
+            
+            attemptedContent[0] = attemptReadText(fileMap, file.getOriginalFilename() + ".txt");
+            attemptedContent[1] = FsUtils.changeExtension(file.getOriginalFilename(), ".txt");
+            attemptedContent[2] = FsUtils.changeExtension(file.getOriginalFilename(), ".TXT");
 
-            if (fileMap.containsKey(txtName)) {
-                try {
-                    content = FsUtils.decodeBytesWithCharset(fileMap.get(txtName).getBytes());
-                    newFile.setContent(content);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-            txtName = FsUtils.changeToTxtExtension(file.getOriginalFilename());
-
-            if (fileMap.containsKey(txtName)) {
-                try {
-                    content = FsUtils.decodeBytesWithCharset(fileMap.get(txtName).getBytes());
-                    newFile.setContent(content);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            for (String attempt : attemptedContent) {
+                if (attempt != null)
+                    content = attempt;
             }
 
             if (fileName.endsWith(".html")) {
@@ -194,5 +184,17 @@ public class FileCrud {
         }
 
         return "OK";
+    }
+
+    private String attemptReadText(HashMap<String, MultipartFile> fileMap, String fileName) {
+        String content = null;
+        if (fileMap.containsKey(fileName)) {
+            try {
+                content = FsUtils.decodeBytesWithCharset(fileMap.get(fileName).getBytes());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return content;
     }
 }

src/main/java/com/info25/journalindex/controllers/Search.java
@@ -55,7 +55,10 @@ public class Search {
         TagSearchOptions tagSearchOptions = so.getTags();
         String searchQuery = so.getQuery();
 
-        SolrSelectQuery selectQuery = new SolrSelectQuery();
+        SolrSelectQuery selectQuery = new SolrSelectQuery()
+            .setHl("true")
+            .setStart(page * 10)
+            .setHlFl("content");
         SolrQueryAssembler assembler = new SolrQueryAssembler();
         if (boundsQuery != null) {
 
@@ -65,6 +68,7 @@ public class Search {
                     boundsQuery.get("boundsNeLat").asDouble() + "," +
                     boundsQuery.get("boundsNeLng").asDouble() + "]");
             selectQuery.setRows(2147483647);
+            selectQuery.setHl("false");
         }
 
         if (tagSearchOptions.getTags().size() > 0) {
@@ -87,10 +91,7 @@ public class Search {
             selectQuery.setQ(searchQuery);
         }
 
-        SolrFileResponse resp = fileRepository.solrQueryForApi(selectQuery
-                .setStart(page * 10)
-                .setHl("true")
-                .setHlFl("content"));
+        SolrFileResponse resp = fileRepository.solrQueryForApi(selectQuery);
 
         for (FileSearchDto f : resp.getFiles()) {
             backlinkRepository.populateBacklinks(f);

src/main/java/com/info25/journalindex/services/SolrClient.java
@@ -73,6 +73,8 @@ public class SolrClient {
         query.put("fq", q.getFq());
         query.put("wt", "json");
         query.put("q.op", "AND");
+        query.put("hl.bs.type", "WORD");
+        query.put("hl.fragsize", 300);
 
         String response = sendHttpRequest("/solr/" + coreName + "/select", root.toString());
         ObjectMapper responseJson = new ObjectMapper();

src/main/java/com/info25/journalindex/util/FsUtils.java
@@ -47,14 +47,14 @@ public class FsUtils {
         return new String(bytes, Charset.forName(charset));
     }
 
-    public static String changeToTxtExtension(String path) {
-        if (path.endsWith(".txt")) {
+    public static String changeExtension(String path, String newExtension) {
+        if (path.endsWith(newExtension)) {
             return path;
         }
         if (path.contains(".")) {
-            return path.substring(0, path.lastIndexOf('.')) + ".txt";
+            return path.substring(0, path.lastIndexOf('.')) + newExtension;
         } else {
-            return path + ".txt";
+            return path + newExtension;
         }
     }
 }

commit 437c4e20d309d43c42e5cb14f3f1c15d3f1d1396

Fix bug reading .txt file for indexing content introduced with 1d52b00

src/main/java/com/info25/journalindex/controllers/FileCrud.java
@@ -149,16 +149,15 @@ public class FileCrud {
             newFile.setContent("");
 
             // attempt to read both a.pdf.txt and a.txt (as an example)
-            String content = "";
             String[] attemptedContent = new String[3];
             
             attemptedContent[0] = attemptReadText(fileMap, file.getOriginalFilename() + ".txt");
-            attemptedContent[1] = FsUtils.changeExtension(file.getOriginalFilename(), ".txt");
-            attemptedContent[2] = FsUtils.changeExtension(file.getOriginalFilename(), ".TXT");
+            attemptedContent[1] = attemptReadText(fileMap, FsUtils.changeExtension(file.getOriginalFilename(), ".txt"));
+            attemptedContent[2] = attemptReadText(fileMap, FsUtils.changeExtension(file.getOriginalFilename(), ".TXT"));
 
             for (String attempt : attemptedContent) {
                 if (attempt != null)
-                    content = attempt;
+                    newFile.setContent(attempt);
             }
 
             if (fileName.endsWith(".html")) {

src/main/java/com/itextpdf/research/xfdfmerge/XfdfMerge.java
@@ -151,10 +151,11 @@ public class XfdfMerge {
             PdfPopupAnnotation pdfPopupAnnot = new PdfPopupAnnotation(readAnnotRect(popup));
             String openProp = popup.getAttributeValue("open");
             boolean open = "yes".equals(openProp);
-            pdfPopupAnnot.setOpen(open)
-                    .setFlags(XfdfObjectReadingUtils
-                            .convertFlagsFromString(popup.getAttributeValue(XfdfConstants.FLAGS)));
+            //pdfPopupAnnot.setOpen(open)
+            //       .setFlags(XfdfObjectReadingUtils
+            //                .convertFlagsFromString(popup.getAttributeValue(XfdfConstants.FLAGS)));
             parent.setPopup(pdfPopupAnnot);
+			pdfPopupAnnot.setFlag(PdfAnnotation.NO_ROTATE);
             pdfDocument.getPage(page).addAnnotation(pdfPopupAnnot);
         }
     }
@@ -307,8 +308,11 @@ public class XfdfMerge {
             switch (annotName) {
                 case XfdfConstants.TEXT:
                     PdfTextAnnotation pdfTextAnnotation = new PdfTextAnnotation(readAnnotRectAndResize(annotObject));
+					
                     addCommonAnnotationAttributes(pdfTextAnnotation, annotObject, color);
                     addMarkupAnnotationAttributes(pdfTextAnnotation, annotObject);
+					
+					
 
                     String icon = annotObject.getAttributeValue(XfdfConstants.ICON);
                     if ("Comment".equals(icon)) {
@@ -323,6 +327,8 @@ public class XfdfMerge {
                     if (stateModelString != null) {
                         pdfTextAnnotation.setStateModel(new PdfString(stateModelString));
                     }
+					
+					pdfTextAnnotation.setFlag(PdfAnnotation.NO_ROTATE);
 
                     page = readAnnotPage(annotObject);
                     pdfDocument.getPage(page).addAnnotation(pdfTextAnnotation);

src/main/resources/templates/annotation_viewers/html.html
@@ -14,7 +14,7 @@
 	/*<![CDATA[*/
 	var date = [[${date}]];
 	$('img').each(function(){ this.src='/api/files/getFile/' + date + '/' + this.getAttribute('src') });
-	$('href').each(function(){ this.src='/api/files/getFile/' + date + '/' + this.getAttribute('href') });
+	$('a').each(function(){ this.src='/api/files/getFile/' + date + '/' + this.getAttribute('href') });
 	$(document).ready(function () {
 		var content = $('body').annotator();
 		content.annotator('addPlugin', 'Store', {