a2faf33b2f896f501901366587adb8a03f1ef41d
Yuyang Liu <yuyangliu12@gmail.com>
2025-07-22 14:06:25 -0400

    Authentication
    Wasn't too bad

--- src/main/java/com/info25/journalindex/config/SecurityConfig.java
+++ src/main/java/com/info25/journalindex/config/SecurityConfig.java
@@ -5,31 +5,61 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+/**
+ * This configures security for the application. Our main goal with authentication
+ * is NOT to create a multi user application -- it only supports one journal, and
+ * major rearchitecting would be needed in order to support multiple users each with
+ * their own journals. Our only goal is to secure the one journal with a password.
+ */
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF protection for simplicity
                 .authorizeHttpRequests(auth -> auth
-                        .anyRequest().permitAll() // Allow all requests without authentication
+                        // access react app & api endpoints should req authentication
+                        // login pages OK w/o auth
+                        .requestMatchers("/app/**").authenticated()
+                        .requestMatchers("/api/**").authenticated()
+                        .requestMatchers("/").authenticated()
+                        .requestMatchers("/static/**").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/loginBg.jpg").permitAll()
                 )
-                .formLogin(Customizer.withDefaults())
+                .formLogin(f -> f.loginPage("/login").loginProcessingUrl("/login"))
                 .headers(headers -> headers.disable())
+                .csrf(csrf -> csrf.disable()) // Disable CSRF protection for simplicity
                 .build();
+    }
 
-                
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    UserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("pass"))
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
     public CorsFilter corsFilter() {
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true); // you USUALLY want this
---
+++ src/main/java/com/info25/journalindex/controllers/Login.java
@@ -0,0 +1,97 @@
+package com.info25.journalindex.controllers;
+
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ * Controllers for login page. Also responsible for the random login page image.
+ *
+ * To customize login bg: create "loginbg" folder in resources folder, then add
+ * jpg images, only.
+ *
+ * Created: 7/22/2025
+ */
+@Controller
+public class Login {
+    ArrayList<String> loginBgs = new ArrayList<String>();
+    boolean useDefaultBg = false;
+
+    /**
+     * initing the class involves checking what images are in the loginbg folder
+     * in the resources folder of the app
+     */
+    public Login() {
+        try {
+            File loginBgFolder = ResourceUtils.getFile("classpath:loginbg");
+            if (loginBgFolder.exists() && loginBgFolder.isDirectory()) {
+                File[] files = loginBgFolder.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isFile() && file.getName().endsWith(".jpg")) {
+                            // this is a login bg, add to list
+                            loginBgs.add(file.getAbsolutePath());
+                        }
+                    }
+                }
+                // no login bgs -- use default
+                if (loginBgs.isEmpty()) {
+                    useDefaultBg = true;
+                }
+            } else {
+                // login bg folder doesn't exist -- use default
+                useDefaultBg = true;
+            }
+        } catch (Exception e) {
+            // error -- use default
+            useDefaultBg = true;
+        }
+    }
+
+    /**
+     * login page endpoint
+     * @return login template
+     */
+    @GetMapping("login")
+    public String login() {
+        return "login";
+    }
+
+    /**
+     * Returns random login background image.
+     * @return jpg image
+     */
+    @GetMapping("loginBg.jpg")
+    @ResponseBody // this is a controller class but this function doesn't return template
+    public ResponseEntity<FileSystemResource> loginBg() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setCacheControl("no-cache, no-store, must-revalidate"); // so it's always random!
+        FileSystemResource fsr = null;
+        // when class was created we figured out whether there were login bg images
+        if (useDefaultBg) {
+            try {
+                // default bg image included on every installation
+                fsr = new FileSystemResource(ResourceUtils.getFile("classpath:defaultbg.jpg"));
+            } catch (Exception e) {
+                return null; // Default background not found
+            }
+        } else {
+            // choose random bg
+            int randomIndex = (int) (Math.random() * loginBgs.size());
+            fsr = new FileSystemResource(loginBgs.get(randomIndex));
+        }
+
+        return new ResponseEntity<>(fsr, headers, HttpStatus.OK);
+    }
+}
